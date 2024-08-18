7c50f7ecf52818fb9ea95b964e4cb90b0d16a5f5
Andrew <11490628+andrew000@users.noreply.github.com>
2024-08-18 19:24:01 +0300

    Fix #4 (FTL-Extract can't see nested kwargs) (#5)

--- src/ftl_extract/process/kwargs_extractor.py
+++ src/ftl_extract/process/kwargs_extractor.py
@@ -3,6 +3,36 @@ from fluent.syntax import ast
 from ftl_extract.matcher import FluentKey
 
 
+def _extract_kwargs_from_variable_reference(
+    variable_reference: ast.VariableReference,
+    kwargs: set[str],
+) -> None:
+    kwargs.add(variable_reference.id.name)
+
+
+def _extract_kwargs_from_selector_expression(
+    selector_expression: ast.SelectExpression,
+    kwargs: set[str],
+) -> None:
+    if isinstance(selector_expression.selector, ast.VariableReference):
+        _extract_kwargs_from_variable_reference(selector_expression.selector, kwargs)
+
+    for variant in selector_expression.variants:
+        for placeable in variant.value.elements:
+            if isinstance(placeable, ast.Placeable):
+                _extract_kwargs_from_placeable(placeable, kwargs)
+
+
+def _extract_kwargs_from_placeable(placeable: ast.Placeable, kwargs: set[str]) -> None:
+    expression = placeable.expression
+
+    if isinstance(expression, ast.VariableReference):
+        _extract_kwargs_from_variable_reference(expression, kwargs)
+
+    elif isinstance(expression, ast.SelectExpression):
+        _extract_kwargs_from_selector_expression(expression, kwargs)
+
+
 def extract_kwargs(key: FluentKey) -> set[str]:
     kwargs: set[str] = set()
 
@@ -14,14 +44,6 @@ def extract_kwargs(key: FluentKey) -> set[str]:
 
     for placeable in key.translation.value.elements:
         if isinstance(placeable, ast.Placeable):
-            expression = placeable.expression
-
-            if isinstance(expression, ast.VariableReference):
-                kwargs.add(expression.id.name)
-
-            elif isinstance(expression, ast.SelectExpression) and isinstance(
-                expression.selector, ast.VariableReference
-            ):
-                kwargs.add(expression.selector.id.name)
+            _extract_kwargs_from_placeable(placeable, kwargs)
 
     return kwargs

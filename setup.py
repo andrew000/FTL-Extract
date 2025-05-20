29eac0af6c246cec06d176eeeff4658565a8a983
andrew000 <11490628+andrew000@users.noreply.github.com>
2025-05-20 14:37:34 +0300

    Update ci.yml

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
+from os import getenv
+
 from setuptools import setup
 from setuptools_rust import RustBin, Strip
 
-setup(
-    package_dir={"ftl_extract": "src/ftl_extract"},
-    rust_extensions=[
+rust_extensions = []
+
+# Read environment variable `BUILD_RUST_IMPL`, if set to "1" build the Rust implementation
+if getenv("BUILD_RUST_IMPL", "0") == "1":
+    rust_extensions.append(
         RustBin(
             "fast-ftl-extract",
             path="src/cli/Cargo.toml",
             strip=Strip.All,
             debug=False,
         )
-    ],
+    )
+
+setup(
+    package_dir={"ftl_extract": "src/ftl_extract"},
+    rust_extensions=rust_extensions,
 )

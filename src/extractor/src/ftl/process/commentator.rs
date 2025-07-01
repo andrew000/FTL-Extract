4a71ac8cba3feeb715c01f4c8a324a2965e9a6d9
andrew000 <11490628+andrew000@users.noreply.github.com>
2025-07-01 11:59:13 +0300

    Fix commentator.rs

--- src/extractor/src/ftl/process/commentator.rs
+++ src/extractor/src/ftl/process/commentator.rs
@@ -8,10 +8,18 @@ pub(crate) fn comment_ftl_key(key: &mut FluentKey) {
     match &key.entry {
         FluentEntry::Message(message) => {
             ser.serialize_message(message);
-            let raw_entry = ser.into_serialized_text();
-            key.entry = FluentEntry::Comment(Comment {
-                content: vec![raw_entry],
-            });
+            let raw_entry: String = ser.into_serialized_text();
+            let mut content: Vec<String> = raw_entry.lines().map(str::to_string).collect();
+
+            if let Some((pre_last, last)) = content.len().checked_sub(2).and_then(|_| {
+                let last = content.pop()?;
+                let pre_last = content.pop()?;
+                Some((pre_last, last))
+            }) {
+                content.push(format!("{pre_last}{last}"));
+            }
+
+            key.entry = FluentEntry::Comment(Comment { content });
         }
         FluentEntry::Term(term) => {
             ser.serialize_term(term);

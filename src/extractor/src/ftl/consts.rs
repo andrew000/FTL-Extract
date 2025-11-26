ed4eb7c9903ad740496de1ebd798b77f23a73278
Andrew <11490628+andrew000@users.noreply.github.com>
2025-11-26 14:26:47 +0200

    Remove lazy_static

--- src/extractor/src/ftl/consts.rs
+++ src/extractor/src/ftl/consts.rs
@@ -1,33 +1,40 @@
 use hashbrown::HashSet;
-use lazy_static::lazy_static;
+use std::sync::LazyLock;
 
 pub const I18N_LITERAL: &str = "i18n";
 pub const GET_LITERAL: &str = "get";
 pub const PATH_LITERAL: &str = "_path";
 pub const DEFAULT_FTL_FILENAME: &str = "_default.ftl";
 
-lazy_static! {
-    pub static ref DEFAULT_I18N_KEYS: HashSet<String> = HashSet::from([
+pub static DEFAULT_I18N_KEYS: LazyLock<HashSet<String>> = LazyLock::new(|| {
+    HashSet::from([
         I18N_LITERAL.to_string(),
         "L".to_string(),
         "LazyProxy".to_string(),
-        "LazyFilter".to_string()
-    ]);
-    pub static ref DEFAULT_EXCLUDE_DIRS: HashSet<String> = HashSet::from([
+        "LazyFilter".to_string(),
+    ])
+});
+
+pub static DEFAULT_EXCLUDE_DIRS: LazyLock<HashSet<String>> = LazyLock::new(|| {
+    HashSet::from([
         "**/.venv/**".to_string(),
         "**/venv/**".to_string(),
         "**/.git/**".to_string(),
         "**/__pycache__/**".to_string(),
         "**/.pytest_cache/**".to_string(),
-    ]);
-    pub static ref DEFAULT_IGNORE_ATTRIBUTES: HashSet<String> = HashSet::from([
+    ])
+});
+
+pub static DEFAULT_IGNORE_ATTRIBUTES: LazyLock<HashSet<String>> = LazyLock::new(|| {
+    HashSet::from([
         "set_locale".to_string(),
         "use_locale".to_string(),
         "use_context".to_string(),
-        "set_context".to_string()
-    ]);
-    pub static ref DEFAULT_IGNORE_KWARGS: HashSet<String> = HashSet::new();
-}
+        "set_context".to_string(),
+    ])
+});
+
+pub static DEFAULT_IGNORE_KWARGS: LazyLock<HashSet<String>> = LazyLock::new(|| HashSet::new());
 
 #[derive(PartialEq, Clone, Debug, clap::ValueEnum)]
 pub enum CommentsKeyModes {
